edbc02047e2bfe36e73b899ea62276efe7dc4eec
Zhenqing Li <lizhenqing.fudan@gmail.com>
2015-01-11 13:50:23 -0500

    The python script is able to deconvolute peaks and calculate Mn, Mw and PDI for each components.

--- GPC_Fit.py
+++ GPC_Fit.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 import os
-from scipy import fftpack, stats
+from scipy import stats
 from lmfit.models import GaussianModel
 
 # This is the place where global variable is defined:
@@ -10,7 +10,44 @@ infile = 'ZHIL1669.arw' # Input file name for Empower Raw data
 bl_start = 1.
 bl_end = 15. # Stand and End point for baseline correction
 start = 17. # user-input term for the start of peak recognization
-end = 32.   # user-input term for the end of peak recognization
+end = 33.   # user-input term for the end of peak recognization
+amp_threshold = 20 # Peak threshold to identify as peak
+# Function definition
+
+def add_peak_info(p_loc, p_amp, p_num):
+    '''
+    This is the function to add the peak information to the p_list dictionary object
+    Usage:
+    peak = add_peak_info(p_loc, p_amp, p_left, p_right, index, p_num)
+    '''
+    peak_name = 'g' + str(p_num) + '_'
+    peak_location = peak_name + 'center'
+    peak_amp = peak_name + 'amplitude'
+    peak = dict(zip([peak_name,peak_location,peak_amp],[p_num,p_loc,p_amp]))
+    return peak
+def gaussian (x,A,mu,sigma):
+    '''
+    This is the function to build the matrix to plot the individual peak,
+    Usage: y = gaussian (x, A, mu, sigma)
+    where x is the data (ndarray object); A, mu and sigma is amplitude, mean and standard deviation in 
+    f(x; A, \\mu, \\sigma) = \\frac{A}{\\sigma\\sqrt{2\\pi}} e^{[{-{(x-\\mu)^2}/{{2\\sigma}^2}}]},
+    '''
+    res = A/(sigma*np.sqrt(2*np.pi))*np.exp(-(x-mu)**2/(2*sigma**2))
+    return res
+
+def calibration(time):
+    '''
+    This is the fuction to calculate the Log Mw from the Elution time
+    Usage:
+    Mp_Log = calibration(time)
+    '''
+    a = 2.27E1
+    b = -1.77E0
+    c = 6.21E-2
+    d = -8.15E-4
+    res = a + b * time + c * time**2 + d * time**3
+    return res
+
 
 # os.chdir('C:\\Users\\zhil\\Documents\\ZQ_Programs\\Coding')
 if (sys.platform != 'linux'):
@@ -45,116 +82,43 @@ y = (y - y_drifting)
 
 # The problem now is how to find the initial parameters for peak fit.
 y1 = np.diff(y, 1)
-y1_fft = fftpack.rfft(y1)
-y1_freq = fftpack.rfftfreq(len(y1),x[3]-x[2])
-
-spectrum = y1_fft **2
-cutoff_idx = spectrum < (spectrum.max()/5)
-y1_fft_filtered = y1_fft.copy()
-y1_fft_filtered[cutoff_idx] = 0
-y1_filtered = fftpack.irfft(y1_fft_filtered)
-
-
-
-y2 = np.diff(y, 2)
-y2_fft = fftpack.rfft(y2)
-y2_freq = fftpack.rfftfreq(len(y2),x[3]-x[2])
-
-spectrum = y2_fft **2
-cutoff_idx = spectrum < (spectrum.max()/5)
-y2_fft_filtered = y2_fft.copy()
-y2_fft_filtered[cutoff_idx] = 0
-y2_filtered = fftpack.irfft(y2_fft_filtered)
-
-
 
 # The next step is to find out the zero points for 1st and 2nd degree of differentiation...
 
 x_start = np.where(x==start)[0]
 x_end = np.where(x==end)[0]
 peak_location = []
-peak_inflection_left = []
-peak_inflection_right = []
 amp = []
-amp_threshold = 20
-del_loc = []
+
 previous = start
-for i in range(x_start,x_end-1):
-    if (y1[i]*y1[i+1] < sys.float_info.epsilon):  # Found that it is better not to smooth
-        if (x[i]-previous > 1.) and (y[i] > amp_threshold):
+for i in range(x_start-1,x_end-1):
+    if (y1[i] > sys.float_info.epsilon) and (y1[i+1] <sys.float_info.epsilon):  
+    # Found that it is better not to smooth
+        if (y[i] > amp_threshold):
             peak_location.append(x[i])
             amp.append(y[i])
             previous = x[i]
         continue
-    if (y2_filtered[i-1]*y2_filtered[i] < sys.float_info.epsilon):
-        if (y2_filtered[i-1] > sys.float_info.epsilon):
-            peak_inflection_left.append(x[i+2])
-        else:
-            peak_inflection_right.append(x[i+2])
 # Then, I need to clean the peak location data
-
-
-print('peak locations are: {0}'.format(peak_location))
-print('peak left inflections are: {0}'.format(peak_inflection_left))
-print('peak right inflections are: {0}'.format(peak_inflection_right))
-print('peak amplitudes are: {0}'.format(amp))
 peaks = []
 
+num = 1
+for i in range(len(peak_location)):
+    peaks.append(add_peak_info(peak_location[i], amp[i],num))
+    num += 1
 
-# Iterate for a peak location/width/amplitude
-def add_peak_info(p_loc, p_amp, p_left, p_right, p_num):
-    '''
-    This is the function to add the peak information to the p_list dictionary object
-    Usage:
-    peak = add_peak_info(p_loc, p_amp, p_left, p_right, index, p_num)
-    '''
-    peak_name = 'g' + str(p_num) + '_'
-    peak_location = peak_name + 'center'
-    peak_width = peak_name + 'sigma'
-    peak_amp = peak_name + 'amplitude'
-    peak = dict(zip([peak_name,peak_location,peak_width,peak_amp],[p_num,p_loc,p_right-p_left,p_amp]))
-    return peak
+# TODO: Here the A parameter considering the std. Manually input now but need 
+# to be changed.
 
-
-num = 0
-
-#TODO: Here is the last break should let the circle go to the next i circle (looking for next left inflection)
-try:
-    for i in range(len(peak_inflection_left)):
-        left = peak_inflection_left[i]
-        for j in range(len(peak_inflection_right)):
-            if (peak_inflection_right[j] > left):
-                right = peak_inflection_right[j]
-                for k in range(len(peak_location)):
-                    if ((left <= peak_location[k]) and (peak_location[k] <= right)):
-                        num += 1
-                        print('num is {0}'.format(num))
-                        peaks.append(add_peak_info(peak_location[k],amp[k],left,right,num)) # call the function to add peak info to the dictionary
-                        peak_location.pop(k)
-                        amp.pop(k)
-                        peak_inflection_left.pop(i)
-                        peak_inflection_right.pop(j)
-                        break
-                peak_inflection_right.pop(j)
-        peak_inflection_right.pop(j)
-    peak_inflection_left.pop(i)
-except IndexError:
-    pass
-                    
-
-for i in range(1, num+1):
+for i in range(1, num):
     name = 'g' + str(i) + '_'
     x2 = peaks[i-1][name+'center']
-    y2 = peaks[i-1][name+'amplitude']
-    displace = peaks[i-1][name+'sigma']
-    print('name is {0}'.format(name))
-    plt.plot((x2,x2),(0,y2),'-')
-#    plt.plot((x2-displace,x2-displace),(0,y2),'--')
-#    plt.plot((x2+displace,x2+displace),(0,y2),'--')
+    sigma = float(input("The sigma of peak {0} is".format(name)))
+    y2 = peaks[i-1][name+'amplitude']/(sigma*np.sqrt(2*np.pi))
+    plt.plot((x2,x2),(0,y2*sigma*np.sqrt(2*np.pi)),'-')
 
 plt.plot(x[x_start:x_end],y[x_start:x_end])
 plt.show()
-
 print(peaks) 
    
 
@@ -181,20 +145,37 @@ pars['g3_center'].set(27, min=25, max=29)
 pars['g3_sigma'].set(0.5, min=0.1)
 pars['g3_amplitude'].set(40, min=20)
 mod = gauss1 + gauss2 + gauss3
-
-
-
-#out = mod.fit(y, pars, x=x)
 out = mod.fit(y[x_start:x_end], pars, x=x[x_start:x_end])
 print(out.fit_report(min_correl=0.5))
 
-comps = out.eval_components(x=x[x_start:x_end])
+#comps = out.eval_components(x=x[x_start:x_end])
+fitted_val = out.best_values
+yp1 = gaussian(x[x_start:x_end],fitted_val['g1_amplitude'],fitted_val['g1_center'],fitted_val['g1_sigma'])
+yp2 = gaussian(x[x_start:x_end],fitted_val['g2_amplitude'],fitted_val['g2_center'],fitted_val['g2_sigma'])
+yp3 = gaussian(x[x_start:x_end],fitted_val['g3_amplitude'],fitted_val['g3_center'],fitted_val['g3_sigma'])
+
+
 
 plt.plot(x[x_start:x_end],y[x_start:x_end])
 plt.plot(x[x_start:x_end], out.best_fit, 'r-')
-plt.plot(x[x_start:x_end],comps['g1_'],'--',x[x_start:x_end],comps['g2_'],'--',x[x_start:x_end],comps['g3_'],'--')
+plt.plot(x[x_start:x_end],yp1,'--',x[x_start:x_end],yp2,'--',x[x_start:x_end],yp3,'--')
 plt.show()
-print(out.best_values)
+
+for i in range(1, num):
+    name = 'g' + str(i) + '_'
+    peaks[i-1][name+'center'] = fitted_val[name+'center']
+    peaks[i-1][name+'area'] = fitted_val[name+'amplitude']
+    peaks[i-1][name+'peakM'] = calibration(fitted_val[name+'center'])
+    M_sigma = (calibration((peaks[i-1][name+'center']+fitted_val[name+'sigma']))
+            - calibration((peaks[i-1][name+'center']-fitted_val[name+'sigma'])))
+    M_sigma /= 2
+    print(M_sigma)
+    peaks[i-1][name+'Mn'] = peaks[i-1][name+'peakM']*np.exp(-(M_sigma**2)/2)
+    peaks[i-1][name+'Mw'] = peaks[i-1][name+'peakM']*np.exp(+(M_sigma**2)/2)
+    peaks[i-1][name+'PDI'] = np.exp(+(M_sigma**2))
+    
+print(peaks)    
+
 
 
 
